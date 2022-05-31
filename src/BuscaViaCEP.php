ea92462dee9edade1547d24634ef925355bdb3b8
Heron Santos <heronsantos@outlook.com>
2022-05-30 23:20:56 -0300

    Ajustes

--- src/BuscaViaCEP.php
+++ src/BuscaViaCEP.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Jarouche\ViaCEP;
+
 /**
  * Classe buscaViaCEP
  * Classe Abstrata para definição de similaridades entre as demais classes
@@ -15,36 +17,36 @@ abstract class buscaViaCEP implements ViaCEPInterface
      * @var string CEP_SITE 
      */
     const CEP_SITE = 'https://viacep.com.br/ws/';
-    
+
     /** 
      * 
      *Constante utilizada para dizer o sub-endereço da requisição
      *p.ex para a requisição https://viacep.com.br/ws/cep_a_buscar/querty/ o valor da variável será '/querty/'
      *Deve ser especificada nas classes filhas que formalmente farão a implementação do método de busca 
      * @var string CEP_METHOD 
-    */
+     */
     const CEP_METHOD = '';
     /** 
      * Armazena cep pré-formatado para busca p.ex 09190099
      * @var string $cep 
      */
     protected $cep;
-     /** 
-      *Armazena o retorno bruto da requisição
-      *@var string $results_string 
+    /** 
+     *Armazena o retorno bruto da requisição
+     *@var string $results_string 
      */
     protected $results_string;
     /** 
-      *Parâmetros extra para requisição
-      *@var string $outros_parametros 
+     *Parâmetros extra para requisição
+     *@var string $outros_parametros 
      */
     protected $outros_parametros;
-     /**
+    /**
      * Método validaCEP
      * Método para a formatação e validação do cep a ser pesquisado
      * @param string $cep
      * @return void;
-      * @throws Exception
+     * @throws Exception
      */
     protected function validaCEP($cep)
     {
@@ -54,19 +56,19 @@ abstract class buscaViaCEP implements ViaCEPInterface
         }
         $this->cep = $formated_cep;
     }
-    
+
     /**
      * Método buscaInfoCEP
      * Método para fazer a requisição e alementar a propriedade results_string
      * @param string $outros_parametros
      * Parâmetro para colocar varáveis extra na url de chamada
      * @return void;
-      * @throws Exception
+     * @throws Exception
      */
 
     protected function buscaInfoCEP()
     {
-        
+
         /*
         $this->results_string = file_get_contents(
                                     self::CEP_SITE . 
@@ -75,19 +77,18 @@ abstract class buscaViaCEP implements ViaCEPInterface
                                     $this->outros_parametros
         );
         */
-        
+
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_HEADER, false);
-        $url = self::CEP_SITE . $this->cep . static::CEP_METHOD.$this->outros_parametros;
+        $url = self::CEP_SITE . $this->cep . static::CEP_METHOD . $this->outros_parametros;
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSLVERSION,3); 
+        // curl_setopt($ch, CURLOPT_SSLVERSION, 3);
         $this->results_string = curl_exec($ch);
         if (curl_errno($ch)) {
             throw new \Exception(curl_error($ch));
         }
         curl_close($ch);
-        
     }
     /**
      * Método fazRequisicao
@@ -97,25 +98,24 @@ abstract class buscaViaCEP implements ViaCEPInterface
      * Parâmetro para colocar varáveis extra na url de chamada
      * @return void;
      * @throws Exception
-    */
+     */
     protected function fazRequisicaoFacade($cep)
     {
         $this->validaCEP($cep);
         $this->buscaInfoCEP();
-    } 
-    
-    
-    
+    }
+
+
+
     public function retornaConteudoRequisicao()
-     {  
-        if($this->results_string == ""){
+    {
+        if ($this->results_string == "") {
             throw new \Exception('Não houve requisição através do método retornaCEP');
         }
         return $this->results_string;
-     }
-     
-     
+    }
+
+
 
     abstract public function retornaCEP($cep);
-
 }
